e8edde3fddf08f88bb152d6b5dd72605c40f2064
gokhanimral <gimral@gmail.com>
2020-06-24 12:02:16 +0400

    Kafka Write Transform

--- beam-extensions/src/main/java/leap/data/beam/io/LeapKafkaIO.java
+++ beam-extensions/src/main/java/leap/data/beam/io/LeapKafkaIO.java
@@ -4,25 +4,37 @@ import com.google.auto.value.AutoValue;
 import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
 import leap.data.beam.configuration.KafkaPipelineOptions;
 import leap.data.beam.serializer.KafkaGenericRecordDeserializer;
+import leap.data.beam.serializer.KafkaRecordSerializer;
 import leap.data.beam.serializer.LeapAvroGenericCoder;
 import leap.data.framework.extension.confluent.kafka.LeapKafkaAvroDeserializer;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.beam.sdk.coders.AtomicCoder;
 import org.apache.beam.sdk.coders.AvroCoder;
+import org.apache.beam.sdk.coders.KvCoder;
 import org.apache.beam.sdk.io.kafka.KafkaIO;
 import org.apache.beam.sdk.io.kafka.KafkaRecord;
+import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.SerializableFunction;
+import org.apache.beam.sdk.transforms.SimpleFunction;
+import org.apache.beam.sdk.transforms.display.DisplayData;
+import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PBegin;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PDone;
 import org.apache.beam.vendor.guava.v26_0_jre.com.google.common.collect.ImmutableList;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.common.serialization.*;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -31,7 +43,45 @@ import static leap.data.beam.configuration.KafkaPipelineOptions.getKafkaProperti
 import static org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Preconditions.checkArgument;
 
 public class LeapKafkaIO {
-    public static LeapGenericRecordRead readGeneric(){
+
+    public static <K> KafkaIO.Read<K, GenericRecord> readGeneric(KafkaPipelineOptions options,
+                                                                String schemaName) throws Exception{
+        return readGeneric(options, schemaName, null);
+    }
+
+    public static <K> KafkaIO.Read<K, GenericRecord> readGeneric(KafkaPipelineOptions options,
+                                                                Schema readerSchema) throws Exception{
+        return readGeneric(options, null, readerSchema);
+    }
+
+    private static <K> KafkaIO.Read<K, GenericRecord> readGeneric(KafkaPipelineOptions options,
+                                                                String schemaName, Schema readerSchema) throws Exception {
+        Map<String,Object> kafkaProperties = getKafkaProperties(options);
+        if(readerSchema != null){
+            kafkaProperties.put("schema.reader",readerSchema.toString());
+        }
+        try {
+            KafkaIO.Read<K, GenericRecord> reader = KafkaIO.<K, GenericRecord>read()
+                    .withBootstrapServers(options.getKafkaBootstrapServers())
+                    .withConsumerConfigUpdates(kafkaProperties);
+            if(readerSchema != null){
+                reader = reader.withValueDeserializerAndCoder(KafkaGenericRecordDeserializer.class,
+                        LeapAvroGenericCoder.of(readerSchema.toString()));
+            }
+            else{
+                reader = reader.withValueDeserializerAndCoder(KafkaGenericRecordDeserializer.class,
+                        LeapAvroGenericCoder.of(schemaName,kafkaProperties));
+            }
+            return reader;
+            //TODO: Handle Exceptions
+        } catch (IOException | RestClientException e) {
+            e.printStackTrace();
+            throw new Exception(e);
+        }
+
+    }
+
+    public static LeapGenericRecordRead readGenericDefault(){
         return new AutoValue_LeapKafkaIO_LeapGenericRecordRead.Builder()
                 .setTopics(new ArrayList<>())
                 .build();
@@ -43,8 +93,25 @@ public class LeapKafkaIO {
                 .build();
     }
 
+    public static <V> LeapRecordWrite<V> writeDefault(){
+        return new AutoValue_LeapKafkaIO_LeapRecordWrite.Builder<V>()
+                .setTopic("")
+                .build();
+    }
+
+    public static <K,V> KafkaIO.Write<K, V> write(KafkaPipelineOptions options){
+        Map<String,Object> kafkaProperties = getKafkaProperties(options);
+
+        @SuppressWarnings("unchecked")
+        KafkaIO.Write<K,V> writer = KafkaIO.<K,V>write()
+                .withBootstrapServers(options.getKafkaBootstrapServers())
+                .withProducerConfigUpdates(kafkaProperties)
+                .withValueSerializer((Class<? extends Serializer<V>>) new KafkaRecordSerializer<V>().getClass());
+        return writer;
+    }
+
     @AutoValue
-    public abstract static class LeapGenericRecordRead extends PTransform<PBegin,PCollection<KafkaRecord<Long, GenericRecord>>> {
+    public abstract static class LeapGenericRecordRead extends PTransform<PBegin,PCollection<KafkaRecord<String, GenericRecord>>> {
         abstract List<String> getTopics();
         @Nullable
         abstract String getSchemaName();
@@ -57,7 +124,7 @@ public class LeapKafkaIO {
         abstract LeapKafkaIO.LeapGenericRecordRead.Builder toBuilder();
 
         @Override
-        public PCollection<KafkaRecord<Long, GenericRecord>> expand(PBegin input) {
+        public PCollection<KafkaRecord<String, GenericRecord>> expand(PBegin input) {
             checkArgument(
                     input.getPipeline().getOptions() instanceof KafkaPipelineOptions,
                     "PipelineOptions should implement KafkaPipelineOptions");
@@ -67,12 +134,12 @@ public class LeapKafkaIO {
             if(getReaderSchema() != null){
                 kafkaProperties.put("schema.reader",getReaderSchema());
             }
-            PCollection<KafkaRecord<Long, GenericRecord>> stream = null;
+            PCollection<KafkaRecord<String, GenericRecord>> stream = null;
             try {
-                KafkaIO.Read<Long, GenericRecord> reader = KafkaIO.<Long, GenericRecord>read()
+                KafkaIO.Read<String, GenericRecord> reader = KafkaIO.<String, GenericRecord>read()
                         .withBootstrapServers(options.getKafkaBootstrapServers())
                         .withTopics(getTopics())
-                        .withKeyDeserializer(LongDeserializer.class)
+                        .withKeyDeserializer(StringDeserializer.class)
                         .withConsumerConfigUpdates(kafkaProperties);
                 if(getReaderSchema() != null){
                     reader = reader.withValueDeserializerAndCoder(KafkaGenericRecordDeserializer.class,
@@ -241,6 +308,121 @@ public class LeapKafkaIO {
         }
 
     }
+
+    @AutoValue
+    public abstract static class LeapRecordWrite<V extends Object> extends PTransform<PCollection<KV<String, V>>, PDone> {
+        abstract String getTopic();
+        @Nullable
+        abstract SerializableFunction<Map<String, Object>, Producer<String, V>> getProducerFactoryFn();
+        abstract LeapKafkaIO.LeapRecordWrite.Builder<V> toBuilder();
+
+        @Override
+        public PDone expand(PCollection<KV<String, V>> input) {
+            checkArgument(
+                    input.getPipeline().getOptions() instanceof KafkaPipelineOptions,
+                    "PipelineOptions should implement KafkaPipelineOptions");
+
+            KafkaPipelineOptions options = (KafkaPipelineOptions) input.getPipeline().getOptions();
+            Map<String,Object> kafkaProperties = getKafkaProperties(options);
+
+            @SuppressWarnings("unchecked")
+            KafkaIO.Write<String,V> writer = KafkaIO.<String,V>write()
+                    .withBootstrapServers(options.getKafkaBootstrapServers())
+                    .withTopic(getTopic())
+                    .withProducerConfigUpdates(kafkaProperties)
+                    .withKeySerializer(StringSerializer.class)
+                    .withValueSerializer((Class<? extends Serializer<V>>) new KafkaRecordSerializer<V>().getClass());
+
+            if(getProducerFactoryFn() != null){
+                writer = writer.withProducerFactoryFn(getProducerFactoryFn());
+            }
+
+            return input.apply(writer);
+        }
+
+        @AutoValue.Builder
+        abstract static class Builder<V>{
+            abstract LeapKafkaIO.LeapRecordWrite.Builder<V> setTopic(String topic);
+            abstract LeapKafkaIO.LeapRecordWrite.Builder<V> setProducerFactoryFn(
+                    SerializableFunction<Map<String, Object>, Producer<String, V>> fn);
+            abstract LeapKafkaIO.LeapRecordWrite<V> build();
+        }
+
+        /**
+         * Sets the topic to read from.
+         */
+        public LeapKafkaIO.LeapRecordWrite<V> withTopic(String topic) {
+            return toBuilder().setTopic(topic).build();
+        }
+
+        /**
+         * Sets a custom function to create Kafka producer. Primarily used for tests. Default is {@link
+         * KafkaProducer}
+         */
+        public LeapKafkaIO.LeapRecordWrite<V> withProducerFactoryFn(
+                SerializableFunction<Map<String, Object>, Producer<String,V>> producerFactoryFn) {
+            return toBuilder().setProducerFactoryFn(producerFactoryFn).build();
+        }
+
+        /**
+         * Writes just the values to Kafka. This is useful for writing collections of values rather
+         * than {@link KV}s.
+         */
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        public PTransform<PCollection<V>, PDone> values() {
+            return new LeapKafkaIO.KafkaValueWrite<>(this);
+        }
+
+    }
+
+    /**
+     * Same as {@code Write<K, V>} without a Key. Null is used for key as it is the convention is
+     * Kafka when there is no key specified. Majority of Kafka writers don't specify a key.
+     */
+    private static class KafkaValueWrite<V> extends PTransform<PCollection<V>, PDone> {
+        private final LeapKafkaIO.LeapRecordWrite<V> kvWriteTransform;
+
+        private KafkaValueWrite(LeapKafkaIO.LeapRecordWrite<V> kvWriteTransform) {
+            this.kvWriteTransform = kvWriteTransform;
+        }
+
+        @Override
+        public PDone expand(PCollection<V> input) {
+            return input
+                    .apply(
+                            "Kafka values with default key",
+                            MapElements.via(
+                                    new SimpleFunction<V, KV<String, V>>() {
+                                        @Override
+                                        public KV<String, V> apply(V element) {
+                                            return KV.of(null, element);
+                                        }
+                                    }))
+                    .setCoder(KvCoder.of(new LeapKafkaIO.NullOnlyCoder<>(), input.getCoder()))
+                    .apply(kvWriteTransform);
+        }
+
+        @Override
+        public void populateDisplayData(DisplayData.Builder builder) {
+            super.populateDisplayData(builder);
+            kvWriteTransform.populateDisplayData(builder);
+        }
+    }
+
+    private static class NullOnlyCoder<T> extends AtomicCoder<T> {
+        @Override
+        public void encode(T value, OutputStream outStream) {
+            checkArgument(value == null, "Can only encode nulls");
+            // Encode as no bytes.
+        }
+
+        @Override
+        public T decode(InputStream inStream) {
+            return null;
+        }
+    }
+
+
 
 
 }
