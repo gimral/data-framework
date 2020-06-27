f8612284664ea7d585c1ef76baa27f35413c74ce
kubesure <edakghar@gmail.com>
2020-06-27 20:39:40 +0400

    added singlestream example

---
+++ examples/singlestream/src/main/java/leap/data/beam/examples/datatypes/AggregatedProspectCompany.java
@@ -0,0 +1,45 @@
+package leap.data.beam.examples.datatypes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AggregatedProspectCompany {
+
+    private Long id;
+    private List<ProspectCompany> companies;
+
+    public AggregatedProspectCompany() {
+        this.companies = new ArrayList<ProspectCompany>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void addCompany(ProspectCompany company) {
+        this.companies.add(company);
+    }
+
+    public ProspectCompany getProspectCompany(){
+        return companies.get(0);
+    }
+
+    public List<ProspectCompany> getProspectCompanies(){
+        return companies;
+    }
+
+    public void setProspectCompanies(List<ProspectCompany> prospectCompanies){
+        this.companies = prospectCompanies;
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Prospect ID- ").append(id).
+        append(" Results aggregated - ").append(companies.size());
+        return sb.toString();
+    }
+}
---
+++ examples/singlestream/src/main/java/leap/data/beam/examples/datatypes/Prospect.java
@@ -0,0 +1,105 @@
+package leap.data.beam.examples.datatypes;
+
+@SuppressWarnings("unused")
+public class Prospect implements Comparable<Prospect>{
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String cif;
+    private String account;
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private boolean match;
+
+    public Prospect(Long id, String firstName, String lastName, boolean match) {
+        this.id=id;
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.match=match;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setFullName(String fullName){
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public boolean isMatch() {
+        return match;
+    }
+
+    public void setMatch(boolean match) {
+        this.match = match;
+    }
+
+    public void setAccount(String account){
+        this.account = account;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public String getCif(){
+        return cif;
+    }
+
+    public void setCif(String cif){
+        this.cif = cif;
+    }
+
+    public Prospect(){}
+
+    public int compareTo(Prospect prospect){
+           return Long.compare(this.id, prospect.id); 
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Customer CIF- ").append(cif).
+        append(" Id - ").append(id).
+        append(" First Name - ").append(firstName).
+        append(" Last Name - ").append(lastName).
+        append(" isMatch - ").append(match);
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) {
+            return true;
+        } else if (o != null && getClass() == o.getClass()) {
+            Prospect that = (Prospect) o;
+            return ( (this.cif.equals(that.cif)) && (this.id.equals(that.id))); 
+        }
+        return false;
+    }
+}
